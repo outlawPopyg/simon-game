b55064fca345a9e14e9116f79bc667c4b5fec238
Popyg <tyl9ray@yandex.ru>
2022-05-27 00:32:02 +0300

    +rules

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import Menu from "./components/menu/Menu";
 import { BrowserRouter as Router,Route, Routes } from "react-router-dom";
 import App from "./components/App";
 import GameOver from "./components/gameOver/gameOver";
+import Rules from "./components/rules/Rules";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,6 +13,7 @@ ReactDOM.render(
             <Route path={"/"} element={<Menu />} />
             <Route path={"/app"} element={<App />} />
             <Route path={"/go"} element={<GameOver />} />
+            <Route path={"/info"} element={<Rules />} />
         </Routes>
     </Router>
   </React.StrictMode>,
